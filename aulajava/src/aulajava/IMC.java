62dfbb51dbad6737fec77a69c885b16b8fdb369d
jothank <jothank@hotmail.com>
2021-08-04 22:42:39 -0300

    up

--- aulajava/src/aulajava/IMC.java
+++ aulajava/src/aulajava/IMC.java
@@ -1,9 +1,19 @@
 package aulajava;
 
+import javax.swing.JOptionPane;
+
 public class IMC {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String nome;
+		double altura, peso, resultado;
+		
+		nome=JOptionPane.showInputDialog("Digite seu nome: ");
+		peso=Double.parseDouble(JOptionPane.showInputDialog("Digite seu peso: "));
+		altura=Double.parseDouble(JOptionPane.showInputDialog("Digite sua altura: "));
+	
+		
 
 	}
 
