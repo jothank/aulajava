1eeda1db6e2acc11b058e211284c79e6a1f6cad0
jothank <jothank@hotmail.com>
2021-08-04 22:59:11 -0300

    up

--- aulajava/src/aulajava/IMC.java
+++ aulajava/src/aulajava/IMC.java
@@ -13,8 +13,19 @@ public class IMC {
 		peso=Double.parseDouble(JOptionPane.showInputDialog("Digite seu peso: "));
 		altura=Double.parseDouble(JOptionPane.showInputDialog("Digite sua altura: "));
 	
+		resultado=peso/(altura*altura);
 		
-
+		if(resultado<=18.5)	{	
+			System.out.println("Olá " + nome + ", seu resultado é: " + resultado + " Magreza.");
+		} else if (resultado>18.5 && resultado <=24.9) {
+			System.out.println("Olá " + nome + ", seu resultado é: " + resultado + " Normal.");
+		} else if (resultado>18.5 && resultado <=24.9) {
+			System.out.println("Olá " + nome + ", seu resultado é: " + resultado + " Sobrepeso.");
+		} else if (resultado>18.5 && resultado <=24.9) {
+			System.out.println("Olá " + nome + ", seu resultado é: " + resultado + " Obesidade.");
+		} else if (resultado>18.5 && resultado <=24.9) {
+			System.out.println("Olá " + nome + ", seu resultado é: " + resultado + " Obesidade Grave.");
+		}
 	}
 
 }
