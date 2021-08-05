38d83c852e20adac2446c1e730d0b81187c4475f
jothank <jothank@hotmail.com>
2021-08-05 19:20:03 -0300

    up

--- aulajava/src/aulajava_poo/curso.java
+++ aulajava/src/aulajava_poo/curso.java
@@ -1,5 +1,17 @@
 package aulajava_poo;
 
 public class curso {
+	
+	public int codigo;
+	public String nome;
+	public int cargaHoraria;
+	public double preco;
+	
+	public void imprime () {
+		System.out.println(codigo);
+		System.out.println(nome);
+		System.out.println(cargaHoraria);
+		System.out.println(preco);
+	}
 
 }
--- aulajava/src/aulajava_poo/programa.java
+++ aulajava/src/aulajava_poo/programa.java
@@ -3,7 +3,16 @@ package aulajava_poo;
 public class programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		curso curso1 = new curso();
+		curso1.codigo=100;
+		curso1.nome="Jonathan";
+		curso1.preco=600.00;
+		curso1.cargaHoraria=40;
+		
+		
+		
 
 	}
 
