4cb0a74f974ba39bd00d60638e17e4961f6781db
jothank <jothank@hotmail.com>
2021-08-04 23:09:31 -0300

    up

--- aulajava/src/aulajava/IMC.java
+++ aulajava/src/aulajava/IMC.java
@@ -1,5 +1,7 @@
 package aulajava;
 
+import java.text.DecimalFormat;
+
 import javax.swing.JOptionPane;
 
 public class IMC {
@@ -8,6 +10,7 @@ public class IMC {
 		
 		String nome;
 		double altura, peso, resultado;
+		DecimalFormat df =  new DecimalFormat("0.00");
 		
 		nome=JOptionPane.showInputDialog("Digite seu nome: ");
 		peso=Double.parseDouble(JOptionPane.showInputDialog("Digite seu peso: "));
@@ -16,15 +19,15 @@ public class IMC {
 		resultado=peso/(altura*altura);
 		
 		if(resultado<=18.5)	{	
-			System.out.println("Olá " + nome + ", seu resultado é: " + resultado + " considerado Magreza.");
+			System.out.println("Olá " + nome + ", seu resultado é: " + df.format(resultado) + " considerado Magreza.");
 		} else if (resultado>18.5 && resultado <=24.9) {
-			System.out.println("Olá " + nome + ", seu resultado é: " + resultado + " considerado Normal.");
+			System.out.println("Olá " + nome + ", seu resultado é: " + df.format(resultado) + " considerado Normal.");
 		} else if (resultado>24.9 && resultado <=29.9) {
-			System.out.println("Olá " + nome + ", seu resultado é: " + resultado + " considerado Sobrepeso.");
+			System.out.println("Olá " + nome + ", seu resultado é: " + df.format(resultado) + " considerado Sobrepeso.");
 		} else if (resultado>29.9 && resultado <=39.9) {
-			System.out.println("Olá " + nome + ", seu resultado é: " + resultado + " considerado Obesidade.");
+			System.out.println("Olá " + nome + ", seu resultado é: " + df.format(resultado) + " considerado Obesidade.");
 		} else if (resultado>39.9) {
-			System.out.println("Olá " + nome + ", seu resultado é: " + resultado + " considerado Obesidade Grave.");
+			System.out.println("Olá " + nome + ", seu resultado é: " + df.format(resultado) + " considerado Obesidade Grave.");
 		}
 	}
 
