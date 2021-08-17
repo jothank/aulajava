e783d400ff6abf178281241ce5016ba1db656649
Jonathan Carlos Costa <40038663+jothank@users.noreply.github.com>
2021-08-16 23:37:33 -0300

    Add files via upload

---
+++ aulajava/src/UMLCurso2/UMLCurso.java
@@ -0,0 +1,48 @@
+package UMLCurso2;
+
+public class UMLCurso {
+	private int codigo;
+	private String nome;
+	private int cargaHoraria;
+	private double preco;
+	
+ public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+public void imprime() {
+	 System.out.println ("Código....... " + codigo);
+	 System.out.println ("Nome......... " + nome);
+	 System.out.println ("Carga horária. " + cargaHoraria);
+	 System.out.println ("Preço......... " + preco);
+ }
+ public void reajustar (int porcentagem) {
+//se o parâmetro for inteiro, o reajuste é em percentual
+	 preco = preco + (preco * porcentagem)/ 100.0;
+ }
+
+ public void reajustar (double reais) {
+//se o parâmetro for double, o reajuste é em R$ (Reais)
+	 preco = preco + reais;
+ }
+ }
---
+++ aulajava/src/UMLCurso2/UMLCursoPrograma.java
@@ -0,0 +1,15 @@
+package UMLCurso2;
+
+public class UMLCursoPrograma {
+	
+	public static void main(String[] args) {
+		
+	UMLCurso c1 = new UMLCurso();
+	 c1.setCodigo(100);
+	 c1.setNome ("Lógica de programação");
+	 c1.setPreco(600.00);
+	 c1.setCargaHoraria(120);
+	 c1.reajustar(10);
+	 c1.imprime();
+	}
+}
